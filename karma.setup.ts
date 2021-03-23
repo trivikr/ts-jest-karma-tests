6de32686fde3e5eec47bb93baba701fa6603bc27
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-03-22 17:03:53 -0700

    chore: rename karma setup file to JavaScript
    The browser test run shows typescript warning even if
    esModuleInterop is set to true.

--- karma.setup.ts
+++
@@ -1,8 +0,0 @@
-import jest from "jest-mock";
-import expect from "expect";
-
-// @ts-ignore: Type 'ModuleMockerClass' is missing properties from type 'typeof jest'
-window.jest = jest;
-
-// @ts-ignore: Property 'expect' does not exist on type 'Window & typeof globalThis'
-window.expect = expect;
