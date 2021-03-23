451fc44cf2f22771e46321155f73cb073ac26b7e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-03-22 22:52:55 -0700

    chore: attempt to write karma test pollyfill

--- karma.setup.js
+++ karma.setup.js
@@ -2,5 +2,6 @@ import jest from "jest-mock";
 import expect from "expect";
 
 // Missing jest functions should be added here, if required.
+window.test = window.it;
 window.jest = jest;
 window.expect = expect;
